bbd33b99571da236e91b7df44c88acaec85792f8
zhengdexing <570158976@qq.com>
2019-07-02 01:37:53 +0800

    提交已完成的运算

--- controllers/calculator.go
+++ controllers/calculator.go
@@ -1,11 +1,60 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"LoansCalculator/entity"
+	"LoansCalculator/util"
+	"encoding/json"
+	"fmt"
+	"github.com/astaxie/beego"
+	"strconv"
+)
 
 type Calculator struct {
 	beego.Controller
 }
 
 func (c *Calculator) Post() {
+	var calculatorInput entity.CalculatorInput
+	var plans []entity.RepaymentPlan
+	// 获取requestBody
+	data := c.Ctx.Input.RequestBody
+	// json解析成对象
+	err := json.Unmarshal(data, &calculatorInput)
+	if err != nil {
+		result := util.Result(util.ERROR, nil, "请求解析失败！")
+		c.Data["json"] = result
+		c.ServeJSON()
+	}
 
+	fmt.Println("calculatorInput:", calculatorInput)
+
+	// 自投金额 = 总投资X自投比例
+	money := calculatorInput.Investment * (calculatorInput.Proportion / 100)
+	// 贷款金额
+	loanMoney, _ := strconv.ParseFloat(
+		util.BigNumberSub(calculatorInput.Investment, money),
+		64,
+	)
+	// 贷款月数
+	month := calculatorInput.Years * 12
+	calculatorOutputRes := entity.CalculatorOutput{
+		Money:     money,
+		LoanMoney: loanMoney,
+		Month:     month,
+	}
+
+	monthList := util.GetAllMonth(calculatorInput.StartMonth, month)
+
+	for _, value := range monthList {
+		plans = append(plans, entity.RepaymentPlan{Time: value, Money: 123213})
+	}
+
+	outPut := entity.Result{
+		CalculatorOutput: calculatorOutputRes,
+		RepaymentPlan:    plans,
+	}
+
+	result := util.Result(util.SUCCESS, outPut, "success")
+	c.Data["json"] = result
+	c.ServeJSON()
 }
