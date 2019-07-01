c470ec4d5da283fade64918b892db0ed450d0d69
zhengdexing <570158976@qq.com>
2019-07-01 23:44:31 +0800

    获取期间所有年月改为从起始时间开始计算

--- util/time.go
+++ util/time.go
@@ -1,19 +1,32 @@
 package util
 
 import (
+	"github.com/shopspring/decimal"
 	"time"
 )
 
+const (
+	layout = "2006年01月" // 时间格式化格式
+)
+
 // 获取期限了所有年月格式为 xx年xx月
+// startTime 起始时间  2019年10月
 // num 月数
-func getAllMonth (num int) (result []string) {
-	t := time.Now()
-	for i:=0;i<num;i++{
+func getAllMonth(startTime string, num int) (result []string) {
+	x := startTime
+	t, _ := time.Parse(layout, x)
+	for i := 0; i < num; i++ {
 		// 获取下个月时间
 		t = t.AddDate(0, 1, 0)
 		// go 格式化时间2006-01-02 15:04:05 代表yyyy:MM:ss hh:mm:ss
 		// 如果15改成3 会格式化成 12小时制
-		result = append(result, t.Format("2006年01月"))
+		result = append(result, t.Format(layout))
 	}
 	return result
+}
+
+func bigNumberSub(value1 float64, value2 float64) string {
+	x := decimal.NewFromFloat(value1)
+	y := decimal.NewFromFloat(value2)
+	return x.Sub(y).String()
 }
